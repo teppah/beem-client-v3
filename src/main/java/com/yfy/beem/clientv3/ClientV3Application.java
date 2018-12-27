2a5ce17fe71c476e31c27cd749541d2e6b3abe88
teppah <yifengyang01@gmail.com>
2018-12-27 12:34:48 -0500

    Initial intellij commit

---
+++ src/main/java/com/yfy/beem/clientv3/ClientV3Application.java
@@ -0,0 +1,14 @@
+package com.yfy.beem.clientv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientV3Application.class, args);
+    }
+
+}
+
