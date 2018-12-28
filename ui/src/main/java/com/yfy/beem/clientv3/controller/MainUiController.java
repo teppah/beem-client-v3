067feb8b42e4d2e4a84f11e24a033e56035ae07c
teppah <yifengyang01@gmail.com>
2018-12-28 12:28:27 -0500

    created FX controller annotated with @Component for Spring integration

---
+++ ui/src/main/java/com/yfy/beem/clientv3/controller/MainUiController.java
@@ -0,0 +1,28 @@
+package com.yfy.beem.clientv3.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+/**
+ * Main UI controller
+ * */
+@Component
+public class MainUiController {
+    private static final Logger log = LoggerFactory.getLogger(MainUiController.class);
+    @FXML
+    private MenuBar menuBar;
+    @FXML
+    private MenuItem testItem;
+    @FXML
+    private TableView msgTableView;
+
+    public void initialize() {
+        log.info("main controller initialized, {}", this);
+    }
+
+
+}
