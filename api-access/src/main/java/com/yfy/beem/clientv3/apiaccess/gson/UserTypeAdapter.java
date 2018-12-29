c4384fc5c8200a30e14dba9bf1225175da36560b
teppah <yifengyang01@gmail.com>
2018-12-29 13:36:51 -0500

    finished implementing UserTypeAdapter

--- api-access/src/main/java/com/yfy/beem/clientv3/apiaccess/gson/UserTypeAdapter.java
+++ api-access/src/main/java/com/yfy/beem/clientv3/apiaccess/gson/UserTypeAdapter.java
@@ -2,16 +2,19 @@ package com.yfy.beem.clientv3.apiaccess.gson;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
+import com.yfy.beem.clientv3.apiaccess.util.ApiConstants;
 import com.yfy.beem.clientv3.apiaccess.util.PropertyName;
 import com.yfy.beem.clientv3.crypto.CryptoUtils;
 import com.yfy.beem.clientv3.datamodel.User;
 
 import java.io.IOException;
+import java.net.InetAddress;
 
 /**
  * Gson {@link com.google.gson.TypeAdapter} for {@link com.yfy.beem.clientv3.datamodel.User}
- * */
+ */
 public class UserTypeAdapter extends TypeAdapter<User> {
     @Override
     public void write(JsonWriter jsonWriter, User user) throws IOException {
@@ -27,6 +30,30 @@ public class UserTypeAdapter extends TypeAdapter<User> {
 
     @Override
     public User read(JsonReader jsonReader) throws IOException {
-        return null;
+        User.Builder builder = User.builder();
+
+        String fieldName = null;
+
+        jsonReader.beginObject();
+
+        while (jsonReader.hasNext()) {
+            switch (jsonReader.nextName()) {
+                case PropertyName.ID:
+                    builder.id(jsonReader.nextLong());
+                    break;
+                case PropertyName.NAME:
+                    builder.name(jsonReader.nextString());
+                    break;
+                case PropertyName.PUBLIC_KEY:
+                    builder.publicKey(CryptoUtils.stringToPublicKey(
+                            jsonReader.nextString()));
+                    break;
+                case PropertyName.IP_ADDR:
+                    builder.ipAddress(InetAddress.getByName(jsonReader.nextString()));
+                    break;
+            }
+        }
+        jsonReader.endObject();
+        return builder.build();
     }
 }
