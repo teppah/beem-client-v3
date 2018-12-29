a4a700e33769e86ec98672cd3f7071bd04f918c0
teppah <yifengyang01@gmail.com>
2018-12-29 14:43:47 -0500

    initial test implemention of useraccesssor

---
+++ api-access/src/main/java/com/yfy/beem/clientv3/apiaccess/UserApiAccessor.java
@@ -0,0 +1,19 @@
+package com.yfy.beem.clientv3.apiaccess;
+
+import com.yfy.beem.clientv3.apiaccess.service.ApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import retrofit2.Retrofit;
+
+/**
+ * Class used to access the REST api endpoints.
+ * */
+@Service
+public class UserApiAccessor {
+    private ApiService service;
+
+    @Autowired
+    public UserApiAccessor(ApiService service) {
+        this.service = service;
+    }
+}
