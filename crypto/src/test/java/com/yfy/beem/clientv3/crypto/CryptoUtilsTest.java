62f8eb004f1fba7fbb067014301a97dbb1cc8f83
teppah <yifengyang01@gmail.com>
2018-12-29 13:19:39 -0500

    added test for the newly implemented methods

--- crypto/src/test/java/com/yfy/beem/clientv3/crypto/CryptoUtilsTest.java
+++ crypto/src/test/java/com/yfy/beem/clientv3/crypto/CryptoUtilsTest.java
@@ -1,6 +1,8 @@
 package com.yfy.beem.clientv3.crypto;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +16,7 @@ import static org.junit.Assert.*;
 /**
  * Tests for {@link CryptoUtils}
  * */
+@FixMethodOrder(MethodSorters.JVM)
 public class CryptoUtilsTest {
     private static final Logger log = LoggerFactory.getLogger(CryptoUtilsTest.class);
     private static final String TEST_STRING = "TestString 12 345 67890 abcdefg-hijklmno pqrstuvq xyz[]{}'./ ,;`~ABCDEF GHIJKLMNOP QRSTUVWXYZ-_=+";
@@ -42,5 +45,35 @@ public class CryptoUtilsTest {
         byte[] encryptedBytes = CryptoUtils.encrypt(publicKey, TEST_STRING.getBytes());
         byte[] decryptedBytes = CryptoUtils.decrypt(privateKey, encryptedBytes);
         assertEquals(TEST_STRING, new String(decryptedBytes));
+    }
+
+    @Test
+    public void testKeyStringConversion() {
+        KeyPair keyPair = CryptoUtils.generateKeyPair();
+        PublicKey publicKey = keyPair.getPublic();
+        PrivateKey privateKey = keyPair.getPrivate();
+
+        String publicKeyContent = CryptoUtils.keyToString(publicKey);
+        String privateKeyContent = CryptoUtils.keyToString(privateKey);
+
+        assertNotNull(publicKeyContent);
+        System.out.println(publicKeyContent);
+
+        assertNotNull(privateKeyContent);
+        System.out.println(privateKeyContent);
+
+        PrivateKey reEncodedPrivate = CryptoUtils.stringToPrivateKey(privateKeyContent);
+        PublicKey reEncodedPublic = CryptoUtils.stringToPublicKey(publicKeyContent);
+
+        // check if not null
+        assertNotNull(reEncodedPrivate);
+        assertNotNull(reEncodedPublic);
+
+        // check if same
+        assertEquals(privateKey, reEncodedPrivate);
+        assertEquals(publicKey, reEncodedPublic);
+
+
+
     }
 }
