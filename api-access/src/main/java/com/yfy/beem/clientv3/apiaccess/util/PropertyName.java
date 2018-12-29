191481e1eec9175b5824540b3e27e2567d6cae90
teppah <yifengyang01@gmail.com>
2018-12-29 13:19:52 -0500

    created helper class to hold property names

---
+++ api-access/src/main/java/com/yfy/beem/clientv3/apiaccess/util/PropertyName.java
@@ -0,0 +1,13 @@
+package com.yfy.beem.clientv3.apiaccess.util;
+
+/**
+ * Class that holds property names.
+ * */
+public class PropertyName {
+    private PropertyName() {
+    }
+
+    public static final String ID = "id";
+    public static final String NAME = "name";
+    public static final String PUBLIC_KEY = "publicKey";
+}
