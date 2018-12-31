ab66b2e08ca966392b84309d0547914ad4f24c3a
teppah <yifengyang01@gmail.com>
2018-12-30 22:41:33 -0500

    added some injected FXML fields to controller

--- ui/src/main/java/com/yfy/beem/clientv3/controller/MainUiController.java
+++ ui/src/main/java/com/yfy/beem/clientv3/controller/MainUiController.java
@@ -1,16 +1,16 @@
 package com.yfy.beem.clientv3.controller;
 
 import com.yfy.beem.clientv3.apiaccess.UserApiAccessor;
+import com.yfy.beem.clientv3.apiaccess.UserApiAccessorImpl;
 import com.yfy.beem.clientv3.datamodel.User;
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 /**
@@ -21,23 +21,45 @@ public class MainUiController {
     private static final Logger log = LoggerFactory.getLogger(MainUiController.class);
 
 
+    // ==== spring autowired components ====
     private ConfigurableApplicationContext ctx;
-    private UserApiAccessor apiAccessor;
+    private Environment env;
+    private UserApiAccessor userApiAccessor;
+
+    // ==== javafx nodes ====
+    @FXML
+    private MenuBar menuBar;
+    @FXML
+    private Menu connectionMenu;
+    @FXML
+    private MenuItem connectMenuItem;
+    // main table for contacts
+    @FXML
+    private TableView<User> msgTableView;
+    // table for chat history
+    @FXML
+    private TableView<String> chatHistoryTableView;
 
     @Autowired
-    public MainUiController(ConfigurableApplicationContext ctx, UserApiAccessor apiAccessor) {
+    public MainUiController(ConfigurableApplicationContext ctx,UserApiAccessor userApiAccessor, Environment env) {
         this.ctx = ctx;
-        this.apiAccessor = apiAccessor;
+        this.userApiAccessor = userApiAccessor;
+        this.env = env;
     }
 
     public void initialize() {
+        chatHistoryTableView.setSelectionModel(null);
         log.info("main controller initialized, {}", this);
     }
 
-    public void testClick() {
-        log.info("clicked, something happened");
-        log.info("{}", apiAccessor);
-        log.info("{}", ctx);
+    public void showConnectDialog() {
+        log.info("showing connection dialog");
+
+
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+
     }
 
 
