5deaacefdb73bf382df3f7a96224fb62b248f8e4
teppah <yifengyang01@gmail.com>
2018-12-30 22:42:36 -0500

    created UserHelper class tto translate to an ObservableList from a normal list of api-access

---
+++ ui/src/main/java/com/yfy/beem/clientv3/util/UserHelper.java
@@ -0,0 +1,34 @@
+package com.yfy.beem.clientv3.util;
+
+import com.yfy.beem.clientv3.apiaccess.UserApiAccessor;
+import com.yfy.beem.clientv3.datamodel.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Wrapper object that helps interfacing between api-access and the ui module. Contains
+ * methods that wrap lists inside Observable stuff.
+ * */
+@Component
+public class UserHelper {
+    private static final Logger log = LoggerFactory.getLogger(UserHelper.class);
+
+    private UserApiAccessor userApiAccessor;
+
+    private ObservableList<User> currentUsers;
+
+    public UserHelper(UserApiAccessor userApiAccessor) {
+        this.userApiAccessor = userApiAccessor;
+        currentUsers = FXCollections.observableArrayList(
+                userApiAccessor.getAllUsers()
+        );
+    }
+
+    public ObservableList<User> getCurrentUsers() {
+    }
+
+}
